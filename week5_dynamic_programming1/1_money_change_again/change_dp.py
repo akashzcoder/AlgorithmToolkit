1db5b220b3170231f49e2d93b1aafc9fcb283509
Akash Singh <akashsingh09.03.93@gmail.com>
2020-08-25 19:25:10 -0400

    week 5 in progress

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,21 @@
 # Uses python3
+import math
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m // 4
+    money = int(input())
+    denominations = [1, 3, 4]
+    minCoins = [0] + [float('inf')] * money
+    for i in range(1, money + 1):
+        for j in denominations:
+            if i >= j:
+                coins = minCoins[i - j] + 1
+                if coins < minCoins[i]:
+                    minCoins[i] = coins
+    return minCoins[money]
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
-    print(get_change(m))
+    print(get_change(34))
--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -2,9 +2,21 @@
 
 import sys
 
+def edit_distance(a, b, c):
+    dp_result = [[[0 for k in range(cn+1)] for j in range(bn+1)] for i in range(an+1)]
+
+    for x in range(1, an+1):
+        for y in range(1, bn+1):
+            for z in range(1, cn+1):
+                if a[x-1] == b[y-1] and b[y-1] == c[z-1]:
+                    dp_result[x][y][z] = dp_result[x-1][y-1][z-1] + 1
+                else:
+                    dp_result[x][y][z] = max(dp_result[x-1][y][z], dp_result[x][y-1][z], dp_result[x][y][z-1])
+
+    return dp_result
+
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    return edit_distance(a,b,c)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,4 +32,5 @@ if __name__ == '__main__':
     cn = data[0]
     data = data[1:]
     c = data[:cn]
-    print(lcs3(a, b, c))
+    result = lcs3(a, b, c)
+    print(result[an][bn][cn])
